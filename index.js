50faca2350347ee9bc99c7fd4c7de87efe658253
Peter Goes <petergoes@gmail.com>
2017-10-26 09:15:11 +0200

    refactor: split module creation in to loader.js

--- index.js
+++ index.js
@@ -1,27 +1,8 @@
-const {promisify} = require('util');
-const readFile = promisify(require('fs').readFile);
-const cheerio = require('cheerio');
-const path = require('path');
-const templateEngine = require('lodash/template');
+const loader = require('./lib/loader');
 
-const blub = async ({input}) => {
-	const pathObj = path.parse(input);
-	if (pathObj.ext !== '.html') throw new Error('You must specify a html file as input');
+const blub = async ({ input }) => {
+	const modules = await loader(input);
+	console.log(modules);
+};
 
-	const fileContent = await readFile(input, {encoding: 'utf8'});
-	const $ = cheerio.load(fileContent);
-
-	const id = pathObj.name;
-	const attributes = $('template').attr();
-	const sourceTemplate = $('template').html().trim();
-	const template = templateEngine($('template').html().trim());
-	const html = template(attributes);
-
-	const moduleObj = {
-		id, attributes, sourceTemplate, template, html
-	}
-
-	console.log(moduleObj);
-}
-
-module.exports = blub
+module.exports = blub;
