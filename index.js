0987ac45f15e254cee788dbb4e391a5c7054f2ab
Peter Goes <petergoes@gmail.com>
2017-10-27 16:40:21 +0200

    refactor: add glob input

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+const { promisify } = require('util');
+const glob = promisify(require('glob'));
 const loader = require('./lib/loader');
 const { renderModule } = require('./lib/renderer');
 
 const blub = async ({ input }) => {
-	const modules = await loader(input);
+	const files = await glob(input);
+	const modules = await loader(files);
 
-	console.log(renderModule(modules));
+	// console.log(renderModule(modules));
+	console.log(modules);
 };
 
 module.exports = blub;
